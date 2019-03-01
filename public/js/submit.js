4aa41140dfcc13c276634e5b7458ed81a389eb7f
Gareth Williams <gaz492@gmail.com>
2019-03-01 20:59:19 +0000

    Fancy interface

---
+++ public/js/submit.js
@@ -0,0 +1,24 @@
+Vue.prototype.$http = axios;
+new Vue({
+    el: '#app',
+    delimiters: ['${', '}'],
+    data: {
+        url: '',
+        baseUrl: window.location,
+        showShortenedURL: false,
+        shortenData: {}
+    },
+    methods: {
+        processForm: function () {
+            const self = this;
+            console.log(`${this.url} submitted`);
+            this.$http.post('/api/shorten', {
+                originalURL: this.url
+            }).then(function (res) {
+                console.log(res.data);
+                self.shortenData = res.data;
+                self.showShortenedURL = true;
+            })
+        }
+    }
+});
