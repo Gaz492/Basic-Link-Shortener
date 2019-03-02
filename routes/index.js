e88e917602f72455dc2afb04e02f6ea98f175c90
Gareth Williams <gaz492@gmail.com>
2019-03-02 21:55:13 +0000

    Client Side Error Page

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,6 @@ const mongoose = require('mongoose');
 const URLShorten = mongoose.model('URLShorten');
 const router = express.Router();
 
-/* GET home page. */
-// router.get('/', function (req, res, next) {
-//     res.render('index', {title: 'Express'});
-// });
-
-router.get('/error', function (req, res, next) {
-    res.render('error', {title: 'URL Not Found', invalidURL: true})
-});
-
 router.get('/:code', async (req, res, next) => {
     const urlCode = req.params.code;
     const item = await URLShorten.findOne({urlCode: urlCode});
