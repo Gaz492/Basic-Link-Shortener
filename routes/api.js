e9ed10bf033a6ba85ad8048514c38f54cf3fa39e
Gareth Williams <gaz492@gmail.com>
2019-03-03 23:41:39 +0000

    if url exists return code

--- routes/api.js
+++ routes/api.js
@@ -7,18 +7,24 @@ const router = express.Router();
 
 /* GET users listing. */
 router.post('/shorten', async (req, res, next) => {
-    const {originalURL} = req.body;
-    console.log(originalURL);
+    const originalURL = req.body.originalURL.toLowerCase();
     const updatedAt = new Date();
     if (validUrl.isUri(originalURL)) {
-        try {
-            const urlCode = shortId.generate();
-            const itemToBeSaved = {originalURL, urlCode, updatedAt};
 
-            // Add the item to db
-            const item = new URLShorten(itemToBeSaved);
-            await item.save();
-            res.status(200).json(itemToBeSaved);
+        try {
+            URLShorten.findOne({originalURL}, async(err, shortURL) => {
+                if(shortURL){
+                    res.status(200).json({urlCode: shortURL.urlCode});
+                }else{
+                    const urlCode = shortId.generate();
+                    const itemToBeSaved = {originalURL, urlCode, updatedAt};
+
+                    // Add the item to db
+                    const item = new URLShorten(itemToBeSaved);
+                    await item.save();
+                    res.status(200).json(itemToBeSaved);
+                }
+            });
         } catch (err) {
             res.status(401).json('Invalid User Id');
         }
